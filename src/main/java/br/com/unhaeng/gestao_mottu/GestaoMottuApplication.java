6ee2c0f3ea96146146ecd2c8a76cd86d5101c892
laviniapark <laviniasoo03@gmail.com>
2025-09-24 15:12:10 -0300

    Projeto base criado

---
+++ src/main/java/br/com/unhaeng/gestao_mottu/GestaoMottuApplication.java
@@ -0,0 +1,13 @@
+package br.com.unhaeng.gestao_mottu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoMottuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoMottuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/unhaeng/gestao_mottu/GestaoMottuApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unhaeng.gestao_mottu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoMottuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

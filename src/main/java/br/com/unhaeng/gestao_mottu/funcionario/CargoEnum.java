268c0086a97cb2314db6c15197496c20f4dbfd50
laviniapark <laviniasoo03@gmail.com>
2025-09-24 15:13:54 -0300

    Adição dos Enums

---
+++ src/main/java/br/com/unhaeng/gestao_mottu/funcionario/CargoEnum.java
@@ -0,0 +1,9 @@
+package br.com.unhaeng.gestao_mottu.funcionario;
+
+public enum CargoEnum {
+
+    FUNCIONARIO,
+    MECANICO,
+    GESTOR,
+    ADMINISTRADOR
+}
---
+++ src/main/java/br/com/unhaeng/gestao_mottu/moto/StatusEnum.java
@@ -0,0 +1,9 @@
+package br.com.unhaeng.gestao_mottu.moto;
+
+public enum StatusEnum {
+
+    DISPONIVEL,
+    EM_MANUTENCAO,
+    OCUPADO
+
+}

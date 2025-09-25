1742323e2f69dbf78ab8d72772c2d7e161eff9c0
laviniapark <laviniasoo03@gmail.com>
2025-09-25 01:18:33 -0300

    Configuraçoes do I18n adicionadas

---
+++ src/main/java/br/com/unhaeng/gestao_mottu/config/MessageHelper.java
@@ -0,0 +1,19 @@
+package br.com.unhaeng.gestao_mottu.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageHelper {
+
+    private final MessageSource messageSource;
+
+    public MessageHelper( MessageSource messageSource ) {
+        this.messageSource = messageSource;
+    }
+
+    public String get(String code){
+        return messageSource.getMessage(code, null, LocaleContextHolder.getLocale());
+    }
+}

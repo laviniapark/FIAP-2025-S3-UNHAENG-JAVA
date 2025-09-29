370c7dbd420bc3fd4c210a47d3e6de919679c878
laviniapark <laviniasoo03@gmail.com>
2025-09-29 11:44:10 -0300

    Configurações para gerar arquivo .WAR

--- src/main/java/br/com/unhaeng/gestao_mottu/GestaoMottuApplication.java
+++ src/main/java/br/com/unhaeng/gestao_mottu/GestaoMottuApplication.java
@@ -2,9 +2,16 @@ package br.com.unhaeng.gestao_mottu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GestaoMottuApplication {
+public class GestaoMottuApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(GestaoMottuApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestaoMottuApplication.class, args);

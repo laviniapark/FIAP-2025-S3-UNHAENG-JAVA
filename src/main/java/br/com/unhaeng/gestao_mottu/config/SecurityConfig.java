e65129f36957e838d5cf75c52c10b15ad974b71c
BuildTools <unconfigured@null.spigotmc.org>
2025-10-02 05:25:26 -0300

    fix(config): security config dividido para liberar acesso sem login de requisicoes API

--- src/main/java/br/com/unhaeng/gestao_mottu/config/SecurityConfig.java
+++ src/main/java/br/com/unhaeng/gestao_mottu/config/SecurityConfig.java
@@ -2,15 +2,35 @@ package br.com.unhaeng.gestao_mottu.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 public class SecurityConfig {
+
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    @Order(1)
+    SecurityFilterChain api(HttpSecurity http) throws Exception {
+        return http
+                .securityMatcher("/api/**")
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                .build();
+    }
+
+    @Bean
+    @Order(2)
+    SecurityFilterChain web(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests( auth -> auth
+                        .requestMatchers("/", "/login", "/oauth2/**", "/error","/static/**", "/assets/**", "/css/**", "/js/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2Login(login -> login.defaultSuccessUrl("/"))
